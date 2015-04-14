9128853667273c949db53e234f7e226ef758ccac
antonsidorovich <asidorovich.consultant@videologygroup.com>
2015-04-14 15:51:45 +0300

    wait for futures

--- src/main/java/Populator.java
+++ src/main/java/Populator.java
@@ -1,5 +1,7 @@
 import com.couchbase.client.CouchbaseClient;
+import com.couchbase.client.internal.ObserveFuture;
 import net.spy.memcached.PersistTo;
+import net.spy.memcached.internal.OperationFuture;
 
 import java.io.IOException;
 import java.net.URI;
@@ -10,6 +12,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class Populator {
 
@@ -19,7 +22,8 @@ public class Populator {
     public static void main(String[] args) throws Exception {
 
         final ArrayList<URI> nodes = new ArrayList<URI>();
-        nodes.add(URI.create("http://usvalzcbstg01.inf.videologygroup.com:8091/pools"));
+        nodes.add(URI.create("http://localhost:8091/pools"));
+//        nodes.add(URI.create("http://usvalzcbstg01.inf.videologygroup.com:8091/pools"));
 
         CouchbaseClient client = null;
         try {
@@ -27,30 +31,36 @@ public class Populator {
         } catch (IOException e1) {
             e1.printStackTrace();
         }
-        
 
         final ExecutorService executor = Executors.newFixedThreadPool(NUMTREADS);
-        int keys_per_thread = NUMKEYS / NUMTREADS;
 
-        final List<Future<String>> futures = new ArrayList<Future<String>>(NUMTREADS);
+        final List<Future> futures = new ArrayList<Future>(NUMKEYS);
 
         for (int i = 0; i < NUMKEYS; i++) {
-                final Callable<String> worker = new Worker(i, client, true);
-                futures.add(executor.submit(worker));
+            final Callable<OperationFuture> worker = new Worker(i, client, true);
+            futures.add(executor.submit(worker));
         }
 
         try {
+            boolean notCompleted;
+            do {
+                notCompleted = false;
+                for (final Future future : futures) {
+                    if (!((ObserveFuture) future.get()).isDone()) {
+                        notCompleted = true;
+                        break;
+                    }
+                }
+            } while (notCompleted);
+
+            System.out.println("Data populated");
+
+            client.shutdown();
             executor.shutdown();
             executor.awaitTermination(5, TimeUnit.MINUTES);
-
-          //for (final Future<String> future : futures) {
-          //    System.out.println(future.get());
-          //}
-
-      } catch (InterruptedException e) {
-          System.out.println("Ow ow");
-      }
-        System.out.println("Data populated");
+        } catch (InterruptedException e) {
+            System.out.println("Ow ow");
+        }
     }
 }
 
--- src/main/java/Worker.java
+++ src/main/java/Worker.java
@@ -1,35 +1,34 @@
 import com.couchbase.client.CouchbaseClient;
 import net.spy.memcached.PersistTo;
+import net.spy.memcached.internal.OperationFuture;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 
-public class Worker implements Callable<String> {
+public class Worker implements Callable<OperationFuture> {
 
     private int key;
     private CouchbaseClient client;
     private boolean writer;
-    
+
     public Worker(int i, CouchbaseClient client, boolean w) {
         this.key = i;
         this.writer = w;
         this.client = client;
-        
+
     }
 
-    public String call() throws Exception {
+    public OperationFuture call() throws Exception {
         try {
-            
-            
             if (writer) {
-                this.client.set(String.valueOf(key), 1000, "couchbase-couchbase-couchbase-couchuchbase-couchbase-couchbase-couchba", PersistTo.ONE);
-                Thread.sleep(10);
+                return this.client.set(String.valueOf(key), 1000, "couchbase-couchbase-couchbase-couchuchbase-couchbase-couchbase-couchba", PersistTo.ONE);
             } else {
-               this.client.getAndTouch(String.valueOf(key), 1000);
+                return this.client.asyncGetAndTouch(String.valueOf(key), 1000);
             }
-            
+
         } catch (Exception e) {
             System.err.println("Error connecting to Couchbase: " + e.getMessage());
+            return null;
         }
-        return "done";
     }
 }
