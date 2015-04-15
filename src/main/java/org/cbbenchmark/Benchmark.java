fa5efa96be4c38e457872d94aba5a2ccdd3d4a24
antonsidorovich <asidorovich.consultant@videologygroup.com>
2015-04-15 15:08:28 +0300

    benchmark

--- src/main/java/org/cbbenchmark/Benchmark.java
+++ src/main/java/org/cbbenchmark/Benchmark.java
@@ -7,7 +7,7 @@ import java.util.concurrent.*;
 public class Benchmark {
 
     /**
-     * @param args -> num of keys, sleep time, num threads, hostname
+     * @param args -> num of keys, sleep time, num threads, hostname, isGenerator, timeout for read request
      * @throws Exception
      */
     public static String value;
@@ -25,42 +25,43 @@ public class Benchmark {
         final int sleepTime = Integer.valueOf(args[1]);
         final int numThreads = Integer.valueOf(args[2]);
         final String hostName = args[3];
-        final String isGenerator = args[4];
+        final boolean isGenerator = Boolean.valueOf(args[4]);
+        final long timeout = Long.valueOf(args[5]);
 
         final int keys_per_thread = numKeys / numThreads;
 
         final ExecutorService executor = Executors.newFixedThreadPool(numThreads);
         final List<Future> futures = new ArrayList<Future>(numKeys);
 
-        if (isGenerator.equals("true")) {
+        if (isGenerator) {
             for (int i = 0; i < numThreads; i++) {
                 final Callable<Future> worker = new Wgenerator(i * keys_per_thread, i * keys_per_thread + keys_per_thread, sleepTime, value, hostName);
                 futures.add(executor.submit(worker));
             }
         } else {
             for (int i = 0; i < numThreads; i++) {
-                final Callable<Future> worker = new Wbenchmark(i * keys_per_thread, i * keys_per_thread + keys_per_thread, sleepTime, value, hostName);
+                final Callable<Future> worker = new Wbenchmark(i * keys_per_thread, i * keys_per_thread + keys_per_thread, sleepTime, value, hostName, timeout);
                 futures.add(executor.submit(worker));
             }
         }
 
-        try {
-            boolean notCompleted;
-            do {
-                notCompleted = false;
-                for (final Future future : futures) {
-                    if (!((Future) future.get()).isDone()) {
-                        notCompleted = true;
-                        break;
-                    }
-                }
-            } while (notCompleted);
+//        try {
+//            boolean notCompleted;
+//            do {
+//                notCompleted = false;
+//                for (final Future future : futures) {
+//                    if (!((Future) future.get()).isDone()) {
+//                        notCompleted = true;
+//                        break;
+//                    }
+//                }
+//            } while (notCompleted);
 
             executor.shutdown();
 
-        } catch (InterruptedException e) {
-            System.out.println("Ow!");
-        }
+//        } catch (InterruptedException e) {
+//            System.out.println("Ow!");
+//        }
         System.out.println("Done");
     }
 }
--- src/main/java/org/cbbenchmark/Wbenchmark.java
+++ src/main/java/org/cbbenchmark/Wbenchmark.java
@@ -1,12 +1,17 @@
 package org.cbbenchmark;
 
 import com.couchbase.client.CouchbaseClient;
+import net.spy.memcached.CASValue;
+import net.spy.memcached.internal.OperationFuture;
+import net.spy.memcached.transcoders.Transcoder;
 
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class Wbenchmark implements Callable<Future> {
 
@@ -15,37 +20,47 @@ public class Wbenchmark implements Callable<Future> {
     private int sleeptime;
     private String hostname;
     private String value;
+    private long timeout;
 
-    public Wbenchmark(int keySatrt, int keyEnd, int sleepTime, String loadValue, String hostName) {
+    public Wbenchmark(int keySatrt, int keyEnd, int sleepTime, String loadValue, String hostName, long timeout) {
         this.keysatrt = keySatrt;
         this.keyend = keyEnd;
         this.sleeptime = sleepTime;
         this.value = loadValue;
         this.hostname = hostName;
+        this.timeout = timeout;
     }
 
     public Future call() throws Exception {
 
-
         final ArrayList<URI> nodes = new ArrayList<URI>();
         nodes.add(URI.create("http://" + hostname + ":8091/pools"));
 
-        CouchbaseClient client = null;
+        CouchbaseClient client;
+
         try {
             client = new CouchbaseClient(nodes, "default", "");
             int n = 0;
             int m = keyend - keysatrt;
-            
+            int timeouts = 0;
+
             for (int i = keysatrt; i < keyend; i++) {
                 Thread.sleep(sleeptime);
-                String result = (String) client.get(String.valueOf(i));
-                if (result.equals(value)) {
-                    n ++;
+                final OperationFuture<CASValue<Object>> operationFuture = client.asyncGetAndTouch(String.valueOf(i), getUnixEpochInSeconds(2592000000L));
+
+                try {
+                    CASValue<Object> result = operationFuture.get(this.timeout, TimeUnit.MILLISECONDS);
+                    if (result.getValue().equals(value)) {
+                        n++;
+                    }
+                } catch (TimeoutException e1) {
+                    timeouts++;
                 }
             }
-            
-            System.out.println( "Requested keys" + String.valueOf(m));
-            System.out.println( "Received keys" + String.valueOf(n));
+
+            System.out.println("Requested keys: " + m);
+            System.out.println("Received keys: " + n);
+            System.out.println("Timeouts: " + timeouts);
             client.shutdown();
 
         } catch (IOException e1) {
@@ -54,6 +69,15 @@ public class Wbenchmark implements Callable<Future> {
         }
 
         return null;
+    }
+
+    public int getUnixEpochInSeconds(final long ttlInMilliseconds) {
+        if (ttlInMilliseconds <= 0) {
+            return 0;
+        } else {
+            final long ttlInSeconds = TimeUnit.SECONDS.convert(ttlInMilliseconds, TimeUnit.MILLISECONDS);
+            return (int) (ttlInSeconds < 2592000 ? ttlInSeconds : ((System.currentTimeMillis() + ttlInMilliseconds) / 1000L));
+        }
     }
 }
 
