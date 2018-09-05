e0c40a86f13b9cc468574c8f6753640a1ac4379d
shuffle <sfxu@foxmail.com>
2018-09-05 23:19:08 +0800

    Update Wgenerator.java

--- src/main/java/org/cbbenchmark/Wgenerator.java
+++ src/main/java/org/cbbenchmark/Wgenerator.java
@@ -1,12 +1,19 @@
 package org.cbbenchmark;
 
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
 import com.couchbase.client.CouchbaseClient;
+import net.spy.memcached.CASValue;
+import net.spy.memcached.internal.OperationFuture;
+import net.spy.memcached.transcoders.Transcoder;
 
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class Wgenerator implements Callable<Future> {
 
@@ -15,17 +22,21 @@ public class Wgenerator implements Callable<Future> {
     private int sleeptime;
     private String hostname;
     private String value;
+    private long timeout;
+    private MetricRegistry registry;
     private String bucketName;
     private String bucketPwd;
     private String prefix;
     private int loopTimes;
 
-    public Wgenerator(int keySatrt, int keyEnd, int sleepTime, String loadValue, String hostName, String bucketName, String bucketPwd, String prefix, int loopTimes) {
+    public Wgenerator(int keySatrt, int keyEnd, int sleepTime, String loadValue, String hostName, long timeout, MetricRegistry registry, String bucketName, String bucketPwd, String prefix, int loopTimes) {
         this.keysatrt = keySatrt;
         this.keyend = keyEnd;
         this.sleeptime = sleepTime;
         this.value = loadValue;
         this.hostname = hostName;
+        this.timeout = timeout;
+        this.registry = registry;
         this.bucketName = bucketName;
         this.bucketPwd = bucketPwd;
         this.prefix = prefix;
@@ -39,13 +50,25 @@ public class Wgenerator implements Callable<Future> {
         nodes.add(URI.create("http://" + hostname + ":8091/pools"));
 
         CouchbaseClient client;
+        final Timer timer = this.registry.timer("timer");
         try {
             client = new CouchbaseClient(nodes, bucketName, bucketPwd);
+            this.registry.counter("total").inc(loopTimes * (keyend - keysatrt));
 
             for (int loop = 0; loop < loopTimes; loop++) {
                 for (int i = keysatrt; i < keyend; i++) {
                     Thread.sleep(sleeptime);
-                    client.set(prefix + String.valueOf(i), 100000, value);
+                    final OperationFuture<Boolean> operationFuture = client.set(prefix + String.valueOf(i), 100000, value);
+
+                    Boolean result = false;
+                    try (@SuppressWarnings("unused") Timer.Context context = timer.time()) {
+                        result = operationFuture.get(this.timeout, TimeUnit.MILLISECONDS);
+                    } catch (TimeoutException e1) {
+                        this.registry.counter("timeout").inc();
+                    }
+                    if (result) {
+                        this.registry.counter("success").inc();
+                    }
                 }
             }
 
